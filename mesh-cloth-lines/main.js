5c82104ab6c333bbb244ed52ef19dd0d8ea1bc16
sean-kl <sean@kingslandlinassi.com>
2017-11-14 14:25:18 +0000

    Change y start point of lines

--- mesh-cloth-lines/main.js
+++ mesh-cloth-lines/main.js
@@ -3,16 +3,15 @@ window.onload = function () {
         context = canvas.getContext('2d'),
         width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight,
-        space = 60,
         mesh = [],
         arms = [],
         angle = 3,
-        length = 40;
+        length = 60;
 
-    for (var i = 0; i < 50; i++) {
-        var arm = Arm.create(0, space * i, length, angle);
+    for (var i = 0; i < 40; i++) {
+        var arm = Arm.create(0, height/2, length, angle);
         arms.push(arm);
-        for (var j = 1; j < 200; j++) {
+        for (var j = 1; j < 250; j++) {
             arms.push(Arm.create(arms[j - 1].getEndX(), arms[j - 1].getEndY(), length, 3));
             arms[j].parent = arms[j - 1];
         }
@@ -25,8 +24,8 @@ window.onload = function () {
         context.clearRect(0, 0, width, height);
         angle += 1;
 
-        for (var i = 0; i < 50; i++) {
-            for (var j = 0; j < 40; j++) {
+        for (var i = 0; i < 40; i++) {
+            for (var j = 0; j < 250; j++) {
                 mesh[i][j].angle = Math.sin(angle * (Math.random() / 6) - 2.5) * (Math.random() / 8);
                 if (j > 0) {
                     mesh[i][j].x = mesh[i][j - 1].getEndX();
@@ -35,7 +34,6 @@ window.onload = function () {
                 mesh[i][j].render(context);
             }
         }
-
 
         requestAnimationFrame(render)
     }
