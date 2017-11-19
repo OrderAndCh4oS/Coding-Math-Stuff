aa86803ead0bb4a1621fe3753362cc36657090b7
Sarcoma <sarcoma@live.co.uk>
2017-11-19 22:49:23 +0000

    Add flow fields particle two

--- flow-fields-particle-two/main.js
+++ flow-fields-particle-two/main.js
@@ -6,10 +6,15 @@ window.onload = function () {
 
     context.lineWidth = 0.15;
 
-    var a = Math.random() * 4 - 2;
-    var b = Math.random() * 4 - 2;
-    var c = Math.random() * 4 - 2;
-    var d = Math.random() * 4 - 2;
+    var a = Math.random() - 4.5;
+    var b = Math.random() - 4.5;
+    var c = Math.random() * 3 - 2.5;
+    var d = Math.random() * 3 - 2.5;
+
+    console.log('a: ' + a);
+    console.log('b: ' + b);
+    console.log('c: ' + c);
+    console.log('d: ' + d);
 
     var points = [];
     for (var y = 0; y < height; y += 5) {
@@ -26,9 +31,9 @@ window.onload = function () {
         // http://paulbourke.net/fractals/clifford/
 
         // scale down x and y
-        var scale = 0.005;
-        x = (x - width / 2) * scale;
-        y = (y - height / 2) * scale;
+        var scale = 0.001;
+        x = (x - width) * scale;
+        y = (y - height) * scale;
 
         // attactor gives new x, y for old one.
         var x1 = Math.sin(a * y) + c * Math.cos(a * x);
@@ -39,7 +44,7 @@ window.onload = function () {
     }
 
     function render () {
-        for (var i = 0; i < points.length; i++) {
+        for (var i = points.length - 1; i >= 0; i--) {
 
             var value = getValue(points[i].x, points[i].y);
             points[i].vx += Math.cos(value) * 0.3;
@@ -56,10 +61,14 @@ window.onload = function () {
             points[i].vx *= 0.99;
             points[i].vy *= 0.99;
 
-            if (points[i].x > width) points[i].x = 0;
-            if (points[i].y > height) points[i].y = 0;
-            if (points[i].x < 0) points[i].x = width;
-            if (points[i].y < 0) points[i].y = height;
+            if (
+                points[i].x > width ||
+                points[i].y > height ||
+                points[i].x < 0 ||
+                points[i].y < 0
+            ) {
+                points.splice(i, 1);
+            }
         }
         requestAnimationFrame(render);
 
